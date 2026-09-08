4018e802f12ca415917f24ff5921cb178a390b80
Erick Daniel Juarez Gil <erickdanieljuarezgil@gmail.com>
2026-09-08 12:40:30 -0600

    build: add required dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,11 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
 }
