f77b67ed5e92902da48c2847cb290e8e2421b6b5
Erick Daniel Juarez Gil <erickdaniel@MacBook-Pro-de-Erick-2.local>
2026-09-15 15:17:40 -0600

    fix: remove rectangular ripple from assistant button

--- app/src/main/java/com/erickjuarez/rickandmorty/ui/characters/AskRickFloatingButton.kt
+++ app/src/main/java/com/erickjuarez/rickandmorty/ui/characters/AskRickFloatingButton.kt
@@ -1,10 +1,17 @@
 package com.erickjuarez.rickandmorty.ui.characters
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
@@ -16,6 +23,21 @@ fun AskRickFloatingButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val interactionSource = remember {
+        MutableInteractionSource()
+    }
+
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    val scale by animateFloatAsState(
+        targetValue = if (isPressed) {
+            0.7f
+        } else {
+            1f
+        },
+        label = "AskRickButtonScale"
+    )
+
     Image(
         painter = painterResource(
             id = R.drawable.ask_rick_and_morty
@@ -23,12 +45,19 @@ fun AskRickFloatingButton(
         contentDescription = stringResource(
             id = R.string.ask_rick_and_morty
         ),
+        contentScale = ContentScale.Fit,
         modifier = modifier
             .size(
                 width = 156.dp,
                 height = 112.dp
             )
+            .graphicsLayer {
+                scaleX = scale
+                scaleY = scale
+            }
             .clickable(
+                interactionSource = interactionSource,
+                indication = null,
                 role = Role.Button,
                 onClick = onClick
             )
