089b81f1e4f2a7dae31d795fc78c51130b44fe3f
Erick Daniel Juarez Gil <erickdaniel@MacBook-Pro-de-Erick-2.local>
2026-09-15 15:48:16 -0600

    feat: style assistant chat top bar

--- app/src/main/java/com/erickjuarez/rickandmorty/ui/assistant/AssistantChatScreen.kt
+++ app/src/main/java/com/erickjuarez/rickandmorty/ui/assistant/AssistantChatScreen.kt
@@ -3,14 +3,9 @@ package com.erickjuarez.rickandmorty.ui.assistant
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,26 +21,8 @@ fun AssistantChatScreen(
     Scaffold(
         modifier = modifier.fillMaxSize(),
         topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        text = stringResource(
-                            R.string.ask_rick_and_morty
-                        )
-                    )
-                },
-                navigationIcon = {
-                    IconButton(
-                        onClick = onBackClick
-                    ) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = stringResource(
-                                R.string.navigate_back
-                            )
-                        )
-                    }
-                }
+            AssistantTopBar(
+                onBackClick = onBackClick
             )
         }
     ) { innerPadding ->
